c1d9c664f79aafaebbe556152a83e0af1b9b0d5c
Nurik <nturdukulov911@gmail.com>
2022-01-16 19:56:05 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,43 @@
+package com.company;
+
+import java.util.*;
+import java.util.stream.Stream;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        // String name = scan.nextLine();
+        System.out.println("Enter 5 names:");
+
+        ArrayList<String> namesa = new ArrayList<>();
+        namesa.add(scan.next());
+        namesa.add(scan.next());
+        namesa.add(scan.next());
+        namesa.add(scan.next());
+        namesa.add(scan.next());
+        for (int i = 0; i < namesa.size(); i++)
+            System.out.println("List a " + namesa.get(i));
+
+        ArrayList<String> namesb = new ArrayList<>();
+        namesb.add(scan.next());
+        namesb.add(scan.next());
+        namesb.add(scan.next());
+        namesb.add(scan.next());
+        namesb.add(scan.next());
+        for (int i = 0; i < namesb.size(); i++) {
+            System.out.println("List b " + namesb.get(i));
+        }
+        ArrayList<String> namesc = new ArrayList<>();
+        Collections.reverse(namesb);
+        Iterator<String> iteratorA = namesa.iterator();
+        Iterator<String> iteratorB = namesb.iterator();
+        for (int i = 0; i < namesa.size(); i++) {
+            namesc.add(iteratorA.next());
+            namesc.add(iteratorB.next());
+
+        }
+        System.out.println(namesc);
+    }
+}
+
